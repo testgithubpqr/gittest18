ed1b5d841d10d5bdacaed957c81af665a5f5ad17
tester <ashwini.rao2105@gmail.com>
2024-04-04 14:01:13 +0530

    master 2nd commit

--- src/gittest18/demo.java
+++ src/gittest18/demo.java
@@ -6,6 +6,7 @@ public class demo {
 		// TODO Auto-generated method stub
 		System.out.print("master first commit");
 		System.out.print("another first commit");
+		System.out.print("master 2nd commit");
 	}
 
 }
